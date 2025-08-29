aab4771454bf3d9cc353632ea10b0159b73251f2
Parham Ebrahimi <aparhama@gmail.com>
2025-08-29 03:47:08 +0330

    create Userlogin view

--- main/account/views.py
+++ main/account/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import OtpRequestSerializer , UserRegisterSerializer
+from .serializers import OtpRequestSerializer , UserRegisterSerializer , UserLoginSerializer
 from .models import Otp
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class OtpRequestView(generics.CreateAPIView):
     queryset = Otp.objects.all()
@@ -10,3 +11,16 @@ class OtpRequestView(generics.CreateAPIView):
 
 class UserRegisterView(generics.CreateAPIView):
     serializer_class = UserRegisterSerializer
+
+class UserLoginView(generics.CreateAPIView):
+    serializer_class = UserLoginSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "refresh": str(refresh),
+            "access": str(refresh.access_token)
+        },status=status.HTTP_200_OK)
