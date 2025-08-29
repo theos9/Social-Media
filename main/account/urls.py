4a7f007421d7d06e26d6cc2fe914a9578ec92fd9
Parham Ebrahimi <aparhama@gmail.com>
2025-08-30 00:14:37 +0330

    create UserProfile url

--- main/account/urls.py
+++ main/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import OtpRequestView, UserRegisterView, UserLoginView
+from .views import OtpRequestView, UserRegisterView, UserLoginView, UserProfileView
 
 urlpatterns = [
     path('otp/request/', OtpRequestView.as_view(), name='otp-request'),
     path('user/register/', UserRegisterView.as_view(), name='user-register'),
     path('user/login/', UserLoginView.as_view(), name='user-login'),
+    path('user/profile/', UserProfileView.as_view(), name='user-profile'),
+
 ]
