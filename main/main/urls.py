296779b413952fe236b4480f89168f9179f65f3c
Parham Ebrahimi <aparhama@gmail.com>
2025-09-05 06:21:39 +0330

    add account and conversation apps to main urls

--- main/main/urls.py
+++ main/main/urls.py
@@ -3,4 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/account/', include('account.urls')),
+    path('api/conversation/', include('conversations.urls')),
 ]
