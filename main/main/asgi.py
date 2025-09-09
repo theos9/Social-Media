8b8863cabd0990fc687579ca48c09cd789d02101
Parham Ebrahimi <aparhama@gmail.com>
2025-09-09 03:50:34 +0330

    create asgi.py for web socket

--- main/main/asgi.py
+++ main/main/asgi.py
@@ -1,16 +1,17 @@
-"""
-ASGI config for main project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
-from django.core.asgi import get_asgi_application
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+django.setup()
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from conversations.routing import websocket_urlpatterns
+from conversations.middleware import JWTAuthMiddleware
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": JWTAuthMiddleware(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
