a462c5a8361d4826080f85977b9203abea4a0b89
Parham Ebrahimi <aparhama@gmail.com>
2025-09-12 05:04:45 +0330

    create contact url

--- main/account/urls.py
+++ main/account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import OtpRequestView, UserRegisterView, UserLoginView, UserProfileView
+from .views import OtpRequestView, UserRegisterView, UserLoginView, UserProfileView, ContactView
 
 urlpatterns = [
     path('otp/request/', OtpRequestView.as_view(), name='otp-request'),
     path('user/register/', UserRegisterView.as_view(), name='user-register'),
     path('user/login/', UserLoginView.as_view(), name='user-login'),
     path('user/profile/', UserProfileView.as_view(), name='user-profile'),
+    path('user/contacts/', ContactView.as_view(), name='user-contacts'),
 
 ]
